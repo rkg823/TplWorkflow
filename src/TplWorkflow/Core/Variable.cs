8475b22baff0e9cf7b1b60f7cc78605420eb8017
Rupak Ghosh <rkg823@gmail.com>
2021-11-23 17:31:39 +0530

    Update Variable.cs

--- src/TplWorkflow/Core/Variable.cs
+++ src/TplWorkflow/Core/Variable.cs
@@ -6,7 +6,7 @@ namespace TplWorkflow.Core
     public string Name { get; }
     public object Data { get; }
 
-    public Variable(string name, object data)
+    public Varia  ble(string name, object data)
     {
       Name = name;
       Data = data;
